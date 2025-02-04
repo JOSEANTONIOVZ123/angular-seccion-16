1e1e95c80f74652da2ae7e566832f856721ace4b
Jose <josevillodres94@gmail.com>
2025-02-04 13:11:25 +0100

    Seccion-16-CS

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -7,5 +9,18 @@ import { Component } from '@angular/core';
   ]
 })
 export class LoginPageComponent {
+
+  constructor(private AuthService: AuthService,
+    private router: Router
+  ){}
+
+  onLogin():void{
+    this.AuthService.login('jose@gmail.com','123456')
+    .subscribe( user => {
+        this.router.navigate(['/']);
+    });
+
+
+  }
 
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from 'src/environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { Observable, tap, of, map, catchError } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
@@ -23,7 +23,28 @@ export class AuthService {
     return this.http.get<User>(`${this.baseUrl}/users/1`)
     .pipe(
       tap(user => this.user = user),
-      tap(user => localStorage.setItem('token',user.id.toString())),
+      tap(user => localStorage.setItem('token','vhhjc1vu2BGVjJ28$')),
     )
+  }
+
+  checkAuthentication():Observable<boolean> {
+
+    if (!localStorage.getItem('token')) return of(false);
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+    .pipe(
+      tap(user=> this.user = user ),
+      map( user => !!user),
+      catchError(err=> of(false))
+    );
+
+  }
+
+
+  logout(){
+    this.user = undefined;
+    localStorage.clear();
   }
 }
